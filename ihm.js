a021b1448905e5f426db95ce8d82fc8f3e22326d
gaetan <gs99@hotmail.fr>
2022-04-21 14:33:56 +0200

    logique du ihm

---
+++ ihm.js
@@ -0,0 +1,23 @@
+import { Todo } from "./classes/todo.js"
+
+export class Ihm{
+    constructor(){
+        this.todos = []
+    }
+    demarrer(){
+        //ajoute 2 todos dans le tableau
+
+        //affiche le tableau de todos
+
+        //clique sur le bouton valider qui ajoute un todo
+        //dans le tableau et l'affiche dans le tableau
+
+        //clique sur le bouton recherche ca recherche tous les memes titre
+        //dans le tableau de todos et ca les affiches
+
+        //clique sur le bouton supprimer qui supprime le todo
+        // du tableau et l'affiche
+
+        //clique sur la case fait et change le bool du todo
+    }
+}
