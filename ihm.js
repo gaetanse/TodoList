7a178bccae81f5234f57a3d473e8bd454d090d6a
gaetan <gs99@hotmail.fr>
2022-04-21 18:00:17 +0200

    recherche fonctionne

--- ihm.js
+++ ihm.js
@@ -38,14 +38,21 @@ export class Ihm{
         const search = document.getElementById('search')
         const btnSearch = document.getElementById('btnsearch')
         btnSearch.addEventListener('click', (e)=>{
-            const arrays = []
-            for(let i=0;i<this.todos;++i){
-                const { titre } = this.todos[i].method()
-                if(titre===search.value)
-                    arrays.push(this.todos[i])
+            if(search.value===""){
+                this.afficherTodo(this.todos);
             }
-            console.log(arrays)
-            this.afficherTodoRecherche(arrays);
+            else{
+                const arrays = []
+                for(let i=0;i<this.todos.length;++i){
+                    const { titre } = this.todos[i].method()
+                    if(titre===search.value){
+                        console.log(titre+ " / "+search.value)
+                        arrays.push(this.todos[i])
+                    }
+                }
+                console.log(arrays)
+                this.afficherTodo(arrays);
+            }
         })
     }
     supprimerTodo(index){
@@ -53,39 +60,6 @@ export class Ihm{
         this.todos.splice(index, 1);
         console.log(this.todos)
         this.afficherTodo(this.todos)
-    }
-    afficherTodoRecherche(todos){
-        const tableauHtml = document.getElementsByTagName('table')[0]
-        tableauHtml.innerHTML = 
-        `<tr class="table-dark">
-           <th scope="col">Id</th>
-           <th scope="col">Titre</th>
-           <th scope="col">Contenu</th>
-           <th scope="col">Statut</th>
-        </tr>`
-        for(let i=0;i<todos.length;++i){
-            const { id, titre, contenu, statut } = todos[i].method()
-            tableauHtml.innerHTML += 
-            `<tr>
-                <td>${id}</td> 
-                <td>${titre}</td>
-                <td>${contenu}</td> 
-                <td>
-                <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked">
-                    <label class="form-check-label" for="flexSwitchCheckChecked">fait</label>
-                </div>
-                </td>
-                <td>
-                    <button type="button" class="btn btn-danger" id="buttonSupprimer${id}">Supprimer</button>
-                </td> 
-            </tr>`
-            const ButtonSupprimer = document.getElementById('buttonSupprimer'+id)
-            ButtonSupprimer.addEventListener('click', (e)=>{
-                e.preventDefault()
-                this.supprimerTodo(id)
-            })
-        }
     }
     afficherTodo(todos){
         const tableauHtml = document.getElementsByTagName('table')[0]
