cd53554e08a01999b4a8a630391745c9f25cdb6d
Turpin Yannick <TurpinYa@gmail.com>
2022-04-21 15:08:00 +0200

    ajout d'afficher Todo dans tableau

--- ihm.js
+++ ihm.js
@@ -31,7 +31,13 @@ export class Ihm{
     supprimerTodo(){
 
     }
-    afficherTodo(){
+    afficherTodo(todo){
+        this.tableauHtml.innerHTML += `<tr>
+        <td>${id}</td> 
+        <td>${titre}</td>
+        <td>${contenu}</td> 
+        <td>${statut}</td> 
+        </tr>`
 
     }
 }   
